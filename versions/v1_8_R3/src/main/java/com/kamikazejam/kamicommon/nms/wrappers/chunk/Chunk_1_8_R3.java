4912480cb8ab02051db9cb0429cec1a17505368d
Jake Moore <jakealexandermoore@icloud.com>
2024-09-07 00:15:42 -0700

    1.0.6 -Removed unstable NMSChunk api (the withUpdatePackets part of NMSChunk.saveAndRefresh)
    Took 3 minutes

--- versions/v1_8_R3/src/main/java/com/kamikazejam/kamicommon/nms/wrappers/chunk/Chunk_1_8_R3.java
+++ versions/v1_8_R3/src/main/java/com/kamikazejam/kamicommon/nms/wrappers/chunk/Chunk_1_8_R3.java
@@ -89,9 +89,9 @@ public class Chunk_1_8_R3 implements NMSChunkDef {
     }
 
     @Override
-    public void saveAndRefresh(boolean withUpdatePackets) {
+    public void saveAndRefresh() {
         // Do regular save and refresh
-        this.saveAndRefreshI(withUpdatePackets);
+        this.saveAndRefreshI();
 
         // Re-cache the chunk, which fixes a race condition if the chunk was currently in use
         World world = this.getNMSChunkProvider().getNMSWorld().getBukkitWorld();

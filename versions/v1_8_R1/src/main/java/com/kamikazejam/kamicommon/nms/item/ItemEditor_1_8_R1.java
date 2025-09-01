14b619ea44e3d1cc27119d0c437c1dd9f85364f9
Jake-Moore <jakealexandermoore@icloud.com>
2025-08-31 22:27:37 -0700

    1.1.4 - Added AbstractItemEditor.setDamage and AbstractItemEditor.getDamage

--- versions/v1_8_R1/src/main/java/com/kamikazejam/kamicommon/nms/item/ItemEditor_1_8_R1.java
+++ versions/v1_8_R1/src/main/java/com/kamikazejam/kamicommon/nms/item/ItemEditor_1_8_R1.java
@@ -1,6 +1,7 @@
 package com.kamikazejam.kamicommon.nms.item;
 
 import com.kamikazejam.kamicommon.nms.abstraction.item.AbstractItemEditor;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
@@ -8,12 +9,27 @@ import org.jetbrains.annotations.NotNull;
 public class ItemEditor_1_8_R1 extends AbstractItemEditor {
     @Override
     public ItemMeta setUnbreakable(@NotNull ItemMeta meta, boolean unbreakable) {
+        // Pre 1.11 method
         meta.spigot().setUnbreakable(unbreakable);
         return meta;
     }
 
     @Override
     public boolean isUnbreakable(@NotNull ItemMeta meta) {
+        // Pre 1.11 method
         return meta.spigot().isUnbreakable();
+    }
+
+    @Override
+    public ItemStack setDamage(@NotNull ItemStack item, int damage) {
+        // Pre 1.13 method
+        item.setDurability((short) damage);
+        return item;
+    }
+
+    @Override
+    public int getDamage(@NotNull ItemStack item) {
+        // Pre 1.13 method
+        return item.getDurability();
     }
 }

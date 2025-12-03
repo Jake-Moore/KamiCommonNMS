fccb79e52574cd70a155916d5dbb5d232492d6af
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2025-12-02 20:48:15 -0800

    1.2.19 - Updates to 1.21.10 Teleporter_LATEST implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 @Suppress("PropertyName")
-val VERSION = "1.2.19-SNAPSHOT-7" // -SNAPSHOT marks WIP versions (snapshots are not CI published)
+val VERSION = "1.2.19" // -SNAPSHOT marks WIP versions (snapshots are not CI published)
 
 plugins {
     id("com.gradleup.shadow") version "9.2.2" apply false
